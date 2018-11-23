61c88b18b5b8009c4b7dfff3e96a3101cb6dc041
majrul <majrul@gmail.com>
2018-11-23 01:49:50 -0800

    code for the common day layer

---
+++ day5/Product.java
@@ -0,0 +1,36 @@
+package com.cdac.model;
+
+public class Product {
+
+	private int id;
+	private String name;
+	private double price;
+	private int quantity;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
